bac7ad54d6861fd4769968aaaaecf5192179c353
AtumaKen <atumaken@gmail.com>
2021-09-05 14:48:57 +0100

    Added CreditLevelResponseDto

---
+++ src/main/java/com/sabi/agent/core/dto/responseDto/CreditLevelResponseDto.java
@@ -0,0 +1,27 @@
+package com.sabi.agent.core.dto.responseDto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CreditLevelResponseDto {
+    private Long id;
+    private BigDecimal limit;
+    private int repaymentPeriod;
+    private Long agentCategoryId;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private Long createdBy;
+    private Long updatedBy;
+    private Boolean isActive;
+}
