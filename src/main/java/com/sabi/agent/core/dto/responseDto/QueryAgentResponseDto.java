2001a47ff293e18d5c2f5ca90c213093cc3e9aa2
Davyjones <ekenedavidson15@gmail.com>
2021-09-06 13:55:06 +0100

    query agent

---
+++ src/main/java/com/sabi/agent/core/dto/responseDto/QueryAgentResponseDto.java
@@ -0,0 +1,54 @@
+package com.sabi.agent.core.dto.responseDto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class QueryAgentResponseDto {
+
+    private Long id;
+
+    private Long agentCategoryId;
+    private String agentCategory;
+    private long userId;
+    private String user;
+    private String scope;
+    private String referralCode;
+    private String address;
+    private String bvn;
+    private String agentType;
+    private Long creditLimit;
+    private Integer payBackDuration;
+    private String comment;
+    private String cardToken;
+    private String status;
+    private String walletId;
+    private String picture;
+    private Boolean hasCustomizedTarget;
+    private Long creditLevelId;
+    private String creditLevel;
+    private Long idTypeId;
+    private String idType;
+    private Long stateId;
+    private String state;
+    private Long bankId;
+    private String bank;
+    private Long countryId;
+    private String country;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private Long createdBy;
+    private Long updatedBy;
+    private Boolean isActive;
+}
