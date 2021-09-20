c9218fdd180f8b658add8500c294fd2d41d65689
AtumaKen <atumaken@gmail.com>
2021-09-20 18:44:07 +0100

    added wallet dtos

---
+++ src/main/java/com/sabi/agent/core/wallet_integration/response/InitiateTopUpResponse.java
@@ -0,0 +1,21 @@
+package com.sabi.agent.core.wallet_integration.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InitiateTopUpResponse implements IWalletResponse{
+    private BigDecimal amount;
+    private String email;
+    private String pocketReferenceId;
+    private String reference;
+    private String topUpRequestUrl;
+    private String topUpStatus;
+}
---
+++ src/main/java/com/sabi/agent/core/wallet_integration/response/WalletStatusResponse.java
@@ -0,0 +1,23 @@
+package com.sabi.agent.core.wallet_integration.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WalletStatusResponse implements IWalletResponse{
+    private BigDecimal balance;
+    private String firstName;
+    private String fundingLink;
+    private String lastName;
+    private String phoneNumber;
+    private BigDecimal seerBitBalance;
+    private String status;
+    private String userId;
+}
