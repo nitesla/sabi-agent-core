56c05044fdf409ba91d881645b002f1a522daa55
muyiwa-spinel <muyiwa.a@spinel.consulting>
2021-10-25 00:01:00 -0700

    QA fix

--- src/main/java/com/sabi/agent/core/dto/requestDto/EnableDisEnableDto.java
+++ src/main/java/com/sabi/agent/core/dto/requestDto/EnableDisEnableDto.java
@@ -4,10 +4,15 @@ package com.sabi.agent.core.dto.requestDto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.NotNull;
+
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class EnableDisEnableDto {
 
      private Long id;
+     @NotNull
+     @AssertFalse
      private boolean isActive;
 }
