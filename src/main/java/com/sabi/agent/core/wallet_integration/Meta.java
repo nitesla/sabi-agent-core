b7d80bfbe52b4eda8706507214174d1994662636
Davyjones <ekenedavidson15@gmail.com>
2021-10-27 13:24:05 +0100

    new merged

--- src/main/java/com/sabi/agent/core/wallet_integration/Meta.java
+++ src/main/java/com/sabi/agent/core/wallet_integration/Meta.java
@@ -1,7 +1,6 @@
 package com.sabi.agent.core.wallet_integration;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
