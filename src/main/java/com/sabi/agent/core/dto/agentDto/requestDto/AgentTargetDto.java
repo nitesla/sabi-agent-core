e5cbca2c279fe4195345f5be537d89b9d114a390
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2021-10-29 10:36:40 +0100

    +++ test fixes

--- src/main/java/com/sabi/agent/core/dto/agentDto/requestDto/AgentTargetDto.java
+++ src/main/java/com/sabi/agent/core/dto/agentDto/requestDto/AgentTargetDto.java
@@ -1,8 +1,9 @@
 package com.sabi.agent.core.dto.agentDto.requestDto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.sabi.agent.core.models.agentModel.Agent;
 import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class AgentTargetDto {
@@ -11,7 +12,13 @@ public class AgentTargetDto {
     private String name;
     private Long targetId;
     private Long agentId;
+    @NotNull(message = "Min can not be empty")
+    @Min(value = 1, message = "Min can not be less than 1")
     private Integer min;
+    @NotNull(message = "Max can not be empty")
+    @Min(value = 1, message = "Max can not be less than 1")
     private Integer max;
+    @NotNull(message = "SuperMax can not be empty")
+    @Min(value = 1, message = "SuperMax can not be less than 1")
     private Integer superMax;
 }
