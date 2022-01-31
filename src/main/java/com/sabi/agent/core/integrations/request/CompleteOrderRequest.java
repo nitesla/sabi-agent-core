0cd1325b28f9e7ab1a7c164c75199fbcad606863
Kenoly <kelechi.a@spinel.consulting>
2022-01-31 15:01:31 +0100

    added annotations to complete order request

--- src/main/java/com/sabi/agent/core/integrations/request/CompleteOrderRequest.java
+++ src/main/java/com/sabi/agent/core/integrations/request/CompleteOrderRequest.java
@@ -1,7 +1,15 @@
 package com.sabi.agent.core.integrations.request;
 
 import com.sabi.agent.core.integrations.response.Payment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CompleteOrderRequest {
 
     private Integer orderId;
