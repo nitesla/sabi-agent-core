cb5a47773439a49a1afaeb92bea74dc12865d51f
toluadesola <t.toluadesola@gmail.com>
2022-02-17 12:43:42 +0100

    Modified MerchantSignUpRequest class

--- src/main/java/com/sabi/agent/core/merchant_integration/request/MerchantSignUpRequest.java
+++ src/main/java/com/sabi/agent/core/merchant_integration/request/MerchantSignUpRequest.java
@@ -10,39 +10,39 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MerchantSignUpRequest {
-    //    private AgentCustomer agentCustomer;
-    //{
-//   "firstName":"Joy",
-//   "lastName":"Jane",
-//   "email":"ue1u@gmaiul.com",
-//   "country":"NG",
-//   "phoneNumber":"08097891894",
-//   "businessName":"Teslim124",
-//   "streetAddress":"Close 55, house 17, Sattelite town, Lagos",
-//   "businessPhoneNumber":"+2348097897894",
-//   "state":"Bayelsa",
-//   "lga":"Sagbama",
-//   "msisdn":"+2348097897894",
-//   "password":"123456",
-//   "businessCategories":[
-//
-//   ],
-//   "referralCode":null
-//}
+
     private List<String> businessCategories;
+
+    private String buildingName;
+    private String businessName;
+    private String communityName;
+    private String countryCode;
+    private String createdBy;
+    private String createdDate;
+    private String deliveryInstruction;
+    private String direction;
+    private String email;
     private String firstName;
     private String lastName;
-    private String username;
-    private String email;
-    private String country;
-    private String phoneNumber;
-    private String businessName;
-    private String streetAddress;
-    private String businessPhoneNumber;
-    private String state;
+    private int latitude;
     private String lga;
-    //id of agent creating the merchant
-    private String agentId;
+    private int longitude;
     private String msisdn;
+    private String nearestLandmark;
+    private String otherNames;
     private String password;
+    private String phoneNumber;
+    private String referralCode;
+    private String registeredBy;
+    private String registrationChannel;
+    private String shopNumber;
+    private String state;
+    private String streetAddress;
+    private String streetName;
+    private String streetNumber;
+    private String userTitle;
+
+    private String username;
+    private String businessPhoneNumber;
+    private String agentId;
 }
