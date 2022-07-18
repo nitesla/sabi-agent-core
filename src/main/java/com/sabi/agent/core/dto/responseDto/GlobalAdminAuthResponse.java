99a82f495155126a85d199191777ab8b6aaeecb6
Kenoly <kelechi.a@spinel.consulting>
2022-07-18 20:07:06 +0100

    Permissions

--- src/main/java/com/sabi/agent/core/dto/responseDto/GlobalAdminAuthResponse.java
+++ src/main/java/com/sabi/agent/core/dto/responseDto/GlobalAdminAuthResponse.java
@@ -2,12 +2,14 @@ package com.sabi.agent.core.dto.responseDto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sabi.framework.models.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -36,6 +38,7 @@ public class GlobalAdminAuthResponse {
     private long tokenExpiry;
     @JsonProperty("userId")
     private long userId;
+    private String accessList;
     private Long globalAdminUserId;
 
     private String globalAdminToken;
